4259d7b4a3b82bec6b4a2d08e5ee67c748b8ebf2
Alexey Talerchik <atalerchik@twelvedevs.com>
2024-02-02 00:01:05 +0300

    fix: nvim configuration

---
+++ .config/nvim/lua/plugins/other.lua
@@ -0,0 +1,24 @@
+return {
+	'tpope/vim-surround',
+	'williamboman/mason-lspconfig.nvim',
+	'L3MON4D3/LuaSnip',
+	'hrsh7th/nvim-cmp',
+	{
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+	},
+	'hrsh7th/cmp-nvim-lsp',
+	-- Git
+	{
+		'tpope/vim-fugitive',
+		cmd = 'G', -- Load when the 'G' command is used
+	},
+	-- Visual enhancements and themes
+	'xiyaowong/transparent.nvim',
+	{
+		'ellisonleao/gruvbox.nvim',
+		config = function ()
+			vim.cmd [[colorscheme gruvbox]]
+		end
+	},
+}
---
+++ .config/nvim/lua/settings.lua
@@ -0,0 +1,65 @@
+local opt = vim.opt
+
+-- Enable mouse
+vim.o.mouse = 'a'
+
+-- Set encoding
+vim.o.encoding = 'utf-8'
+
+-- Enable line numbers
+opt.relativenumber = true
+vim.wo.number = true
+
+-- Disable swap file
+vim.o.swapfile = false
+
+-- Set scrolloff
+vim.o.scrolloff = 7
+
+-- Set tabstop, softtabstop, shiftwidth, and expandtab
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+
+-- Enable autoindent
+vim.o.autoindent = true
+vim.o.smartindent = true
+
+-- Set fileformat
+vim.o.fileformat = 'unix'
+
+-- Set numberwidth
+vim.o.numberwidth = 2
+
+-- Enable filetype-specific indent files
+vim.cmd('filetype indent on')
+
+-- Netrw settings
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 3
+
+-- Line wrap 
+opt.wrap = false
+
+-- Search settings
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+opt.incsearch = true
+
+
+-- Vim markdown (vim-markdown-set-header-folding-level)
+vim.g.vim_markdown_folding_level = 6
+
+
+opt.expandtab = false;
+
+-- disable netrw at the very start of init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Apperance
+opt.cursorline = true
+opt.termguicolors = true
