926478a697f356bef8a2b632792da5d7bf81e56e
Alexey Talerchik <atalerchik@twelvedevs.com>
2024-03-01 10:44:09 +0300

    feat: update configuration for obsidian nvim

--- .config/nvim/lua/plugins/obsidian-nvim.lua
+++ .config/nvim/lua/plugins/obsidian-nvim.lua
@@ -1,12 +1,14 @@
 return {
 	"epwalsh/obsidian.nvim",
-	version = "*",  -- recommended, use latest release instead of latest commit
-	lazy = false,
+	tag = "v3.6.1",
+  requires = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+  },
 	config = function ()
 		require("obsidian").setup({
 			dir = "~/vaults/work",
 
-			detect_cwd = false,
 			daily_notes = {
 				folder = "home/daily",
 				date_format = "%Y-%m-%d",
@@ -17,9 +19,10 @@ return {
 			completion = {
 				nvim_cmp = true,
 				min_chars = 2,
-				new_notes_location = "notes_subdir",
 				prepend_note_id = true
 			},
+
+			new_notes_location = "notes_subdir",
 
 			mappings = {
 				["gf"] = {
@@ -28,6 +31,12 @@ return {
 					end,
 					opts = { noremap = false, expr = true, buffer = true },
 				},
+				["<leader>ch"] = {
+					action = function()
+						return require("obsidian").util.toggle_checkbox()
+					end,
+					opts = { buffer = true },
+				},
 			},
 
 			note_id_func = function(title)
@@ -35,6 +44,7 @@ return {
 				if title ~= nil then
 					suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
 				else
+					-- If title is nil, just add 4 random uppercase letters to the suffix.
 					for _ = 1, 4 do
 						suffix = suffix .. string.char(math.random(65, 90))
 					end
@@ -44,15 +54,54 @@ return {
 
 			disable_frontmatter = false,
 
-			note_frontmatter_func = function(note)
-				local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-				if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
-					for k, v in pairs(note.metadata) do
-						out[k] = v
+			  -- Optional, customize how wiki links are formatted.
+				---@param opts {path: string, label: string, id: string|?}
+				---@return string
+				wiki_link_func = function(opts)
+					if opts.id == nil then
+						return string.format("[[%s]]", opts.label)
+					elseif opts.label ~= opts.id then
+						return string.format("[[%s|%s]]", opts.id, opts.label)
+					else
+						return string.format("[[%s]]", opts.id)
 					end
-				end
-				return out
-			end,
+				end,
+
+				-- Optional, customize how markdown links are formatted.
+				---@param opts {path: string, label: string, id: string|?}
+				---@return string
+				markdown_link_func = function(opts)
+					return string.format("[%s](%s)", opts.label, opts.path)
+				end,
+
+				-- Either 'wiki' or 'markdown'.
+				preferred_link_style = "wiki",
+
+				-- Optional, customize the default name or prefix when pasting images via `:ObsidianPasteImg`.
+				---@return string
+				image_name_func = function()
+					-- Prefix image names with timestamp.
+					return string.format("%s-", os.time())
+				end,
+
+				  note_frontmatter_func = function(note)
+    -- Add the title of the note as an alias.
+    if note.title then
+      note:add_alias(note.title)
+    end
+
+    local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+    -- `note.metadata` contains any manually added fields in the frontmatter.
+    -- So here we just make sure those fields are kept in the frontmatter.
+    if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+      for k, v in pairs(note.metadata) do
+        out[k] = v
+      end
+    end
+
+    return out
+  end,
 
 			templates = {
 				subdir = "meta/templates",
@@ -71,14 +120,40 @@ return {
 				}
 			},
 
-			backlinks = {
-				height = 10,
-				wrap = true,
-			},
-
 			follow_url_func = function(url)
 				vim.fn.jobstart({"xdg-open", url})  -- linux
 			end,
+
+			use_advanced_uri = false,
+
+  -- Optional, set to true to force ':ObsidianOpen' to bring the app to the foreground.
+  open_app_foreground = false,
+
+  picker = {
+    -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+    name = "telescope.nvim",
+    -- Optional, configure key mappings for the picker. These are the defaults.
+    -- Not all pickers support all mappings.
+    mappings = {
+      -- Create a new note from your query.
+      new = "<C-x>",
+      -- Insert a link to the selected note.
+      insert_link = "<C-l>",
+    },
+  },
+
+  -- Optional, sort search results by "path", "modified", "accessed", or "created".
+  -- The recommend value is "modified" and `true` for `sort_reversed`, which means, for example,
+  -- that `:ObsidianQuickSwitch` will show the notes sorted by latest modified time
+  sort_by = "modified",
+  sort_reversed = true,
+
+  -- Optional, determines how certain commands open notes. The valid options are:
+  -- 1. "current" (the default) - to always open in the current window
+  -- 2. "vsplit" - to open in a vertical split if there's not already a vertical split
+  -- 3. "hsplit" - to open in a horizontal split if there's not already a horizontal split
+  open_notes_in = "current",
+
 
 			-- Optional, configure additional syntax highlighting / extmarks.
 			-- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
