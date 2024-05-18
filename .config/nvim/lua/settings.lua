3a6c2b69def1eba85ced05e73cbd36ea9f4c7c52
Alexey Talerchik <talerchik@protonmail.com>
2024-05-18 14:13:05 +0000

    feat: use zinit fow work with zsh

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -1,10 +1,10 @@
 local opt = vim.opt
 
 -- Enable mouse
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 
 -- Set encoding
-vim.o.encoding = 'utf-8'
+vim.o.encoding = "utf-8"
 
 -- Enable line numbers
 opt.relativenumber = true
@@ -28,20 +28,20 @@ vim.o.autoindent = true
 vim.o.breakindent = true
 
 -- Set fileformat
-vim.o.fileformat = 'unix'
+vim.o.fileformat = "unix"
 
 -- Set numberwidth
 vim.o.numberwidth = 2
 
 -- Enable filetype-specific indent files
-vim.cmd('filetype indent on')
+vim.cmd("filetype indent on")
 
 -- Netrw settings
 vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
 vim.g.netrw_browse_split = 3
 
--- Line wrap 
+-- Line wrap
 opt.wrap = false
 
 -- Search settings
@@ -50,12 +50,10 @@ opt.smartcase = true
 opt.hlsearch = true
 opt.incsearch = true
 
-
 -- Vim markdown (vim-markdown-set-header-folding-level)
 vim.g.vim_markdown_folding_level = 6
 
-
-opt.expandtab = false;
+opt.expandtab = false
 
 -- disable netrw at the very start of init.lua
 vim.g.loaded_netrw = 1
@@ -64,6 +62,9 @@ vim.g.loaded_netrwPlugin = 1
 -- Apperance
 opt.cursorline = true
 opt.termguicolors = true
+vim.o.hidden = true
+vim.o.updatetime = 300
+vim.o.timeoutlen = 500
 
 -- Enable the sign column to prevent the screen from jumping
 vim.opt.signcolumn = "yes"
@@ -71,8 +72,6 @@ vim.opt.signcolumn = "yes"
 -- Always keep 8 lines above/below cursor unless at start/end of file
 vim.opt.scrolloff = 8
 
-
 vim.o.conceallevel = 2
 vim.api.nvim_set_hl(2, "HelpBar", { link = "Normal" })
 vim.api.nvim_set_hl(2, "HelpStar", { link = "Normal" })
-
